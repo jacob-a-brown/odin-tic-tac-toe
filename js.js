ed609981a74bb25cb61915205afd3bf4edd7860d
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-24 16:39:39 -0600

    work on playing game via prompt

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
+const EMPTY_CELL = " ";
+
 // there can only be one gameboard so make it an IIFE
 const gameboard = (() => {
     // - update the game board with a new piece by setting it 
     // - get the current state of the board
     
     // [column, row]
-    const gameArray = [["","",""], ["","",""], ["","",""]];
+    const gameArray = [[EMPTY_CELL, EMPTY_CELL, EMPTY_CELL], [EMPTY_CELL, EMPTY_CELL, EMPTY_CELL], [EMPTY_CELL, EMPTY_CELL, EMPTY_CELL]];
 
     const setPiece = function (column, row, letter){
         // set a piece on the board
@@ -22,29 +24,58 @@ const gameboard = (() => {
     }    
 })();
 
-function createPlayer(name, letter) {
+function createPlayer(letter) {
     // factory function to do the following:
-    // - name the player
     // - assign the player's letter
     // - set a piece on the game board
 
-    const playPiece = function (column, row) {
-        return [column, row];
+    const getLetter = function () {
+        return letter;
     }
 
     return {
-        name,
-        letter,
-        playPiece
+        getLetter
     }
 }
 
-function createGame(playerOne, playerTwo){
+function createGame(){
     // factory function to creates a game
     // - determines if the game is won
     //   - if the game is not won moves on to the next player
     // - determines if a move is legal (i.e. the spot is empty)
     //   - if a move is illegal then replay the round
+
+    // keep track of whose turn it is, starting with playerX;
+
+    const playerX = createPlayer("X");
+    const playerO = createPlayer("O");
+
+    let currentPlayer = playerX;
+
+    const isMoveLegal = function (column, row){
+        const currentGameboardArray = gameboard.getBoard()
+        if (currentGameboardArray[column, row] === EMPTY_CELL){
+            console.log("go ahead!")
+            return true;
+        } else {
+            console.log("don't go ahead!")
+            return false;
+        }
+    }
+
+    const playPiece = function (column, row){
+        if (isMoveLegal(column, row)){
+            gameboard.setPiece(column, row, currentPlayer.getLetter())
+            if (currentPlayer === playerX){
+                currentPlayer = playerO;
+            } else {
+                currentPlayer = playerX;
+            }
+            console.log(gameboard.getBoard())
+        } else {
+            alert(`[${column}, ${row}] is already taken with ${gameboard.getBoard()}. Play again.`)
+        } 
+    }
 
     const gameOver = function (){
         // returns empty string if game is not over
@@ -57,8 +88,8 @@ function createGame(playerOne, playerTwo){
             const previousCellLetter = currentGameboardArray[column, 0];
             for (let row = 1; row < 3; row++){
                 const currentCellLetter = currentGameboardArray[column][row];
-                if (currentCellLetter === "" || previousCellLetter === ""){
-                    return ""
+                if (currentCellLetter === EMPTY_CELL || previousCellLetter === EMPTY_CELL){
+                    return EMPTY_CELL
                 } else if (currentCellLetter !== previousCellLetter){
                     break;
                 } else if (row === 3){
@@ -73,8 +104,8 @@ function createGame(playerOne, playerTwo){
             const previousCellLetter = currentGameboardArray[0, row];
             for (let column = 1; column < 3; column++){
                 const currentCellLetter = currentGameboardArray[column][row];
-                if (currentCellLetter === "" || previousCellLetter === ""){
-                    return ""
+                if (currentCellLetter === EMPTY_CELL || previousCellLetter === EMPTY_CELL){
+                    return EMPTY_CELL
                 } else if (currentCellLetter !== previousCellLetter){
                     break;
                 } else if (column === 3){
@@ -85,30 +116,33 @@ function createGame(playerOne, playerTwo){
         }
 
         // check crosses
-        if (gamboardArray[0][0] === currentGameboardArray[1][1] && currentGameboardArray[1][1] === currentGameboardArray[2][2] && currentGameboardArray[0][0] !== ""){
+        if (gamboardArray[0][0] === currentGameboardArray[1][1] && currentGameboardArray[1][1] === currentGameboardArray[2][2] && currentGameboardArray[0][0] !== EMPTY_CELL){
             winningLetter = currentGameboardArray[0][0];
             return winningLetter;
-        } else if (currentGameboardArray[0][2] === currentGameboardArray[1][1] && currentGameboardArray[1][1] === currentGameboardArray[2][0] && currentGameboardArray[0][2] !== ""){
+        } else if (currentGameboardArray[0][2] === currentGameboardArray[1][1] && currentGameboardArray[1][1] === currentGameboardArray[2][0] && currentGameboardArray[0][2] !== EMPTY_CELL){
             winningLetter = currentGameboardArray[0][2];
             return winningLetter;
         }
 
-        return "";
+        return EMPTY_CELL;
     }
 
-    const isMoveLegel = function (column, row){
-        const currentGameboardArray = gameboard.getBoard()
-        if (currentGameboardArray[column, row] === ""){
-            return true;
-        } else {
-            return false;
+    const playGame = function() {
+        while(gameOver() === EMPTY_CELL){
+            let column = parseInt(prompt("column? "))
+            let row = parseInt(prompt("row? "))
+
+            playPiece(column, row);
         }
+        console.log(gameOver(), "won the game!")
     }
+
+    return {
+        playGame
+    }
+    
 }
 
-console.log(1)
-console.log(gameboard.getBoard())
-console.log(2)
-gameboard.setPiece(0, 0, "X");
-console.log(3)
-// console.log(gameboard.getBoard())
+
+const currentGame = createGame();
+currentGame.playGame();
